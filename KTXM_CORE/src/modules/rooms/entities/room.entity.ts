164201f052e99dd57efe2e110ea7b61ddcb4179d
ThanhTungEric <99382836+ThanhTungEric@users.noreply.github.com>
2024-08-18 23:12:08 +0700

    pull code fe

--- KTXM_CORE/src/modules/rooms/entities/room.entity.ts
+++ KTXM_CORE/src/modules/rooms/entities/room.entity.ts
@@ -1,1 +1,56 @@
-export class Room {}
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { HydratedDocument } from 'mongoose';
+
+export type RoomDocument = HydratedDocument<Room>;
+
+export class Equipment {
+    @Prop()
+    name: string;
+
+    @Prop()
+    quantity: number;
+}
+
+@Schema({ timestamps: true })
+export class Room {
+    @Prop({ index: true })
+    roomNumber: string;
+
+    @Prop()
+    description: string;
+
+    @Prop()
+    floor: number;
+
+    @Prop()
+    capacity: number;
+
+    @Prop()
+    availableSpot: number;
+
+    @Prop()
+    occupied: boolean;
+
+    @Prop()
+    price: number;
+
+    @Prop({ type: [Equipment] })
+    equipment: Equipment[];
+
+    @Prop()
+    type: string;
+
+    @Prop()
+    gender: string;
+
+    @Prop({ index: true })
+    block: string;
+
+    @Prop()
+    waterNumber: number;
+
+    @Prop()
+    electricityNumber: number;
+}
+
+export const RoomSchema = SchemaFactory.createForClass(Room);
