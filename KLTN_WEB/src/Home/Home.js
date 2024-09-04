a045a6fdbc4a3c2a3c8295b3df7179dc96ccf501
ThanhTungEric <99382836+ThanhTungEric@users.noreply.github.com>
2024-09-04 22:02:30 +0700

    pull new code

--- KLTN_WEB/src/Home/Home.js
+++ KLTN_WEB/src/Home/Home.js
@@ -1,17 +1,26 @@
+import React from "react";
 import style from './Home.module.css';
 import { useNavigate } from 'react-router-dom';
 import Button from '@mui/material/Button';
-import ImportXLSX from './Import-xlsx/ImportXlxs'
+import ImportXLSX from './Import-xlsx/ImportXlxs';
 import ImportRoom from './Import-room/ImportRoom';
-
+import BasicModal from './components/BasicModal';
 import TotalStudent from './components/TotalStudent';
+
 const Home = () => {
+    const [open, setOpen] = React.useState(false);
+
+    const handleOpen = () => setOpen(true);
+    const handleClose = () => setOpen(false);
+
     return (
         <div className={style['home-container']}>
             <div>
                 <TotalStudent />
                 <ImportXLSX />
                 <ImportRoom />
+                <BasicModal handleClose={handleClose} open={open} />
+                <Button onClick={handleOpen}>Click me</Button>
             </div>
         </div>
     );
