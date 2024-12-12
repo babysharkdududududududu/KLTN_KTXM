7f13862b58c0151de4631136af7603bd8e5f836a
ThanhTungEric <99382836+ThanhTungEric@users.noreply.github.com>
2024-12-12 22:18:12 +0700

    fix url image

--- KTXM_CORE/src/config/multer.config.ts
+++ KTXM_CORE/src/config/multer.config.ts
@@ -6,8 +6,8 @@ export const multerConfig = {
     storage: diskStorage({
         destination: (req, file, cb) => {
             const studentId = req.body.userId; // Lấy mã sinh viên từ body
-            const uploadPath = join(__dirname, '..', '..', '..', 'KLTN_WEB', 'public', 'upload', studentId); // Đường dẫn tới thư mục upload trong KLTN_WEB
-
+            //const uploadPath = join(__dirname, '..', '..', '..', 'KLTN_WEB', 'public', 'upload', studentId); // Đường dẫn tới thư mục upload trong KLTN_WEB
+            const uploadPath = join(__dirname, '..', '..', '..', 'KLTN_WEB', 'build', 'upload', studentId); // Đường dẫn tới thư mục build trong KLTN_WEB
             // Tạo thư mục nếu chưa tồn tại
             mkdirSync(uploadPath, { recursive: true });
 
